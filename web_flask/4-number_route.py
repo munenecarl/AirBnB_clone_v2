578b13bb579eec70c2549599e1aee1f3a851b9c6
munenecarl <munenecarl@gmail.com>
2023-12-31 20:23:21 +0300

    Add number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+"""Starts a minimal web app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """displays Hello HBNB"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Displays text in support for C"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """Displays text in support for python"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_display(n):
+    """Displays the number the user has input in the route"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
